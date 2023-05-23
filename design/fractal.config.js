5371d9e4cfb2d9fa440a9d82b0fea36bb1c1e707
Johan N. Martin <jnm+johanmartindev@catenare.com>
2023-05-23 16:50:54 +0000

    Fractal setup

--- design/fractal.config.js
+++ design/fractal.config.js
@@ -1,31 +1,37 @@
-'use strict';
+"use strict";
 
 /*
-* Require the path module
-*/
-const path = require('path');
+ * Require the path module
+ */
+const path = require("path");
 
 /*
  * Require the Fractal module
  */
-const fractal = module.exports = require('@frctl/fractal').create();
+const fractal = (module.exports = require("@frctl/fractal").create());
 
 /*
  * Give your project a title.
  */
-fractal.set('project.title', 'Design');
+fractal.set("project.title", "Design");
 
 /*
  * Tell Fractal where to look for components.
  */
-fractal.components.set('path', path.join(__dirname, 'components'));
+fractal.components.set("path", path.join(__dirname, "components"));
 
 /*
  * Tell Fractal where to look for documentation pages.
  */
-fractal.docs.set('path', path.join(__dirname, 'docs'));
+fractal.docs.set("path", path.join(__dirname, "docs"));
 
 /*
  * Tell the Fractal web preview plugin where to look for static assets.
  */
-fractal.web.set('static.path', path.join(__dirname, 'public'));
+fractal.web.set("static.path", path.join(__dirname, "public"));
+
+/*
+ * Setup nunjucks
+ */
+fractal.components.engine("@frctl/nunjucks"); // register the Nunjucks adapter for your components
+fractal.components.set("ext", ".njk"); // look for files with a .njk file extension
