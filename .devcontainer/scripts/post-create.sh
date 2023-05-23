f73c21794706e83d0a87d332d9f34f7325f866e4
Johan Martin <jnm+johanmartindev@catenare.com>
2023-05-23 18:26:16 +0200

    Setup git config

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -2,5 +2,8 @@
 set -xe
 git config --global user.email "jnm+johanmartindev@catenare.com"
 git config --global user.name "Johan Martin"
+git config --global --unset gpg.ssh.program
 nvm install --lts
+nvm use
+npm i -g @frctl/fractal
 exit
