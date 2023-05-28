d3e58a8b7b60f16da1c511bb1109fe8e069840cb
Johan Martin <jnm+johanmartindev@catenare.com>
2023-05-28 12:22:00 +0200

    Update create script

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -xe
-git config --global user.email "jnm+johanmartindev@catenare.com"
-git config --global user.name "Johan Martin"
-git config --global --unset gpg.ssh.program
 nvm install --lts
 nvm use
 npm i -g @frctl/fractal npm@latest prettier stylelint stylelint-config-standard stylelint-config-prettier stylelint-config-recommended
+git config --global user.email "jnm+johanmartindev@catenare.com"
+git config --global user.name "Johan Martin"
+# git config --global --unset-all gpg.ssh.program
 exit
