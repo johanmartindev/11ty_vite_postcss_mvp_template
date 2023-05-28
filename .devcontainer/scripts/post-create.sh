441ea28c9c8cf661d1bf350791f0217a8147b429
Johan Martin <jnm+johanmartindev@catenare.com>
2023-05-28 11:41:33 +0200

    Update setup script

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -5,5 +5,5 @@ git config --global user.name "Johan Martin"
 git config --global --unset gpg.ssh.program
 nvm install --lts
 nvm use
-npm i -g @frctl/fractal
+npm i -g @frctl/fractal npm@latest prettier stylelint stylelint-config-standard stylelint-config-prettier stylelint-config-recommended
 exit
