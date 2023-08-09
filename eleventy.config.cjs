140d492843659db7981ad9395a7260999905a3a0
Johan N. Martin <jnm+johanmartindev@catenare.com>
2023-08-09 13:43:58 +0000

    Move to main file

---
+++ eleventy.config.cjs
@@ -0,0 +1,58 @@
+const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+const postCss = require("postcss");
+const postcssPresetEnv = require("postcss-preset-env");
+const cssnano = require("cssnano");
+const path = require("path");
+
+const postcssFilter = (cssCode, done) => {
+  // we call PostCSS here.
+  postCss([postcssPresetEnv(), cssnano({ preset: "default" })])
+    .process(cssCode, {
+      // path to our CSS file
+      from: "styles.css",
+    })
+    .then(
+      (r) => done(null, r.css),
+      (e) => done(e, null)
+    );
+};
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setServerPassthroughCopyBehavior('copy');
+  eleventyConfig.addPassthroughCopy({ "src/assets/images": "img" });
+  eleventyConfig.addPassthroughCopy({"src/css": "css"})
+  eleventyConfig.addWatchTarget("src/**/*.scss");
+  eleventyConfig.addNunjucksAsyncFilter("postcss", postcssFilter);
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    serverOptions: {
+      port: 9090,
+    },
+    viteOptions: {
+      clearScreen: true,
+      server: {
+        mode: "development",
+        middlewareMode: true,
+      },
+      appType: "custom",
+      build: {
+        mode: "production",
+        sourcemap: true,
+        manifest: true,
+      },
+      resolve: {
+        alias: {
+          "~icons": path.resolve(__dirname, "node_modules/bootstrap-icons"),
+          "/node_modules": path.resolve(__dirname, "node_modules"),
+        },
+      },
+    },
+  });
+
+  return {
+    templateFormats: ["md", "html", "njk"],
+    dir: {
+      layouts: "_layouts",
+      includes: "_includes",
+    },
+  };
+};
