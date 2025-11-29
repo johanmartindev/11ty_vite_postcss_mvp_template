5d47671953470292528d20556bfda421868d4d30
Johan N. Martin <jnm+johanmartindev@catenare.com>
2025-11-29 12:26:55 +0200

    Chore: cleanup

--- .devcontainer/scripts/post-create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xe
-nvm install --lts
-nvm use
-npm i -g @frctl/fractal npm@latest prettier stylelint stylelint-config-standard stylelint-config-prettier stylelint-config-recommended
-git config --global user.email "jnm+johanmartindev@catenare.com"
-git config --global user.name "Johan Martin"
-# git config --global --unset-all gpg.ssh.program
-exit
--- .gitpod.Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM gitpod/workspace-node-lts
-USER gitpod
-
-RUN sudo apt-get update && \
-    sudo apt-get install -y zsh && \
-    sudo rm -rf /var/lib/apt/lists/*
-
-RUN sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
